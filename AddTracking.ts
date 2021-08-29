8bfc01b8adc988d1e35f613e8987bbddff0328ea
Alicja Slupska <ala.slupska@gmail.com>
2021-08-29 22:18:58 +0200

    AddTracking class done

--- AddTracking.ts
+++ AddTracking.ts
@@ -1,61 +1,58 @@
+// tracker mock
 class MyTracker {
-    /**
-     * Tracks a single click event
-     * @param track_id id of the element that was clicked on
-     */
+    // Tracks a single click event
     static track(track_id: number): void {
         // example implementation
         console.log("Event was tracked for" + track_id);
     }
 }
-
+// meta information for an element to track
 interface TrackingData {
-    name: string;
+    name?: string;
     selector: string;
     id: number;
 }
 
 class ElementTracker {
-    tracked = [];
-    addTracking(trackingData: TrackingData | Array<TrackingData>) {
 
-/*         const apply = obj => {
-            const element = document.querySelector(obj.selector);
-            // let attached = false;
-            // if (element !== null && !attached)
-            if (element !== null && !this.tracked.includes(obj.id)) {
-                // element.setAttribute("attached", "true"); but this will modify html code on the website
-                this.tracked.push(obj.id);
-                element.addEventListener("click", () => {
-                    MyTracker.track(obj.id);
-                });
-            }
-        }
+    // remembers the ids of the elements which are already tracked
+    tracked: Array<number> = [];
 
-        if (Array.isArray(trackingData)) {
-            trackingData.forEach(apply);
-        } else {
-            apply(trackingData);
-        } */
-
-
+    addTracking(trackingData: TrackingData | Array<TrackingData>): boolean {
+        // ensure that trackingData is an array
         if (!Array.isArray(trackingData)) {
-            trackingData = [trackingData]; // put the object into an array
+            trackingData = [trackingData];
         }
 
-        trackingData
-            .forEach(obj => {
-                const element = document.querySelector(obj.selector); // using selector property find HTML element on the website with this selector
+        const elementsFound = trackingData
+            .map(obj => {
+                // using selector property find HTML element on the website with this CSS selector
+                const element = document.querySelector(obj.selector);
+                // add event listner once if the element was found
                 if (element !== null && !this.tracked.includes(obj.id)) {
                     this.tracked.push(obj.id);
                     element.addEventListener("click", () => {
                         MyTracker.track(obj.id);
+
+                        const developer = localStorage.getItem("developer-mode");
+                        if (developer === "true") {
+                            console.log(`Element with ${obj.name ? "name " + obj.name + " and " : ""}tracking id ${obj.id} was clicked!`)
+                        }
                     });
                 }
+                return !!element;
             });
+
+        for (const elemFound of elementsFound) {
+            if (!elemFound) {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
+// example usage
 window.onload = () => {
     // on load
     let trackingData: [
